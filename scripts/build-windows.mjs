f7d446977d2f009da532bd0ee401ae4a43d697f3
Xiaoyu Zhang <zxiaoyu.work@gmail.com>
2026-02-27 13:32:10 -0800

    Switch Windows code signing to Azure Trusted Signing

--- scripts/build-windows.mjs
+++ scripts/build-windows.mjs
@@ -2,14 +2,10 @@
 /* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
 import { build as builder } from 'electron-builder'
 import * as vars from './vars.mjs'
-import { execSync } from 'child_process'
 
 const isTag = (process.env.GITHUB_REF || process.env.BUILD_SOURCEBRANCH || '').startsWith('refs/tags/')
-const keypair = process.env.SM_KEYPAIR_ALIAS
 
 process.env.ARCH = process.env.ARCH || process.arch
-
-console.log('Signing enabled:', !!keypair)
 
 builder({
     dir: true,
@@ -26,38 +22,7 @@ builder({
                 channel: `latest-${process.env.ARCH}`,
             },
         ] : undefined,
-        forceCodeSigning: !!keypair,
-        win: {
-            signtoolOptions: {
-                certificateSha1: process.env.SM_CODE_SIGNING_CERT_SHA1_HASH,
-                publisherName: process.env.SM_PUBLISHER_NAME,
-                signingHashAlgorithms: ['sha256'],
-                sign: keypair ? async function (configuration) {
-                    console.log('Signing', configuration)
-                    if (configuration.path) {
-                        try {
-                            const cmd = `smctl sign --keypair-alias=${keypair} --input "${String(configuration.path)}"`
-                            console.log(cmd)
-                            const out = execSync(cmd)
-                            if (out.toString().includes('FAILED')) {
-                                throw new Error(out.toString())
-                            }
-                            console.log(out.toString())
-                        } catch (e) {
-                            console.error(`Failed to sign ${configuration.path}`)
-                            if (e.stdout) {
-                                console.error('stdout:', e.stdout.toString())
-                            }
-                            if (e.stderr) {
-                                console.error('stderr:', e.stderr.toString())
-                            }
-                            console.error(e)
-                            process.exit(1)
-                        }
-                    }
-                } : undefined,
-            },
-        },
+        forceCodeSigning: false,
     },
 
     publish: (process.env.KEYGEN_TOKEN && isTag) ? 'always' : 'never',
